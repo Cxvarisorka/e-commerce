24761492b938c4c493f0d8cd7e38d21c8b9f2904
skittery268 <sabadzidzikashvili6@gmail.com>
2026-04-30 02:15:28 +0400

    Add email verification in server

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const sendMail = require('../utils/email');
 
 const userSchema = new mongoose.Schema({
     fullname: {
@@ -14,14 +16,18 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, "Password is required!"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
     }
 }, {
     timestamps: true
 });
 
 // Middlewares
-userSchema.pre('save', async function(next) {
-    if(!this.isModified('password')) next();
+userSchema.pre('save', async function() {
+    if(!this.isModified('password')) return;
     this.password = await bcrypt.hash(this.password, 10);
 });
 
@@ -30,8 +36,21 @@ userSchema.methods.comparePassword = async function(candidate) {
     return await bcrypt.compare(candidate, this.password);
 };
 
+// Method to send verification link in user email
+userSchema.methods.sendVerificationLink = function() {
+    const token = jwt.sign({ userId: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES });
+
+    const link = `http://localhost:3000/api/auth/verify-account?token=${token}`;
+
+    const html = `
+        <h1>Verification Link</h1>
+        <a href="${link}">Click here to verify your account!</a>
+    `
+
+    sendMail(this.email, "Verification Link", html);
+};
+
 // Model
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
-
